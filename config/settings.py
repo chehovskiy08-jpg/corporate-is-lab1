c78f379e2b48ea9317e38373744f08585a058d93
Маргарита <rita.limonova@list.ru>
2026-09-22 14:52:30 +0700

    Switch Django to PostgreSQL on port 5434

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "corporate_is",
+        "USER": "corporate_is",
+        "PASSWORD": "corporate_is_password",
+        "HOST": "127.0.0.1",
+"PORT": "5434",
+        "OPTIONS": {
+            "client_encoding": "UTF8",
+        },
     }
 }
 # Password validation
