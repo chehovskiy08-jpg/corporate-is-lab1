e388d3a1c51e1db58d28b13e4e54dee405c9a0b2
Маргарита <rita.limonova@list.ru>
2026-09-22 15:09:06 +0700

    Add Department model, link to Employee

--- employees/models.py
+++ employees/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Employee(models.Model):
     full_name = models.CharField(max_length=200)
     position = models.CharField(max_length=100)
     hired_at = models.DateField()
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="employees",
+    )
 
     def __str__(self):
         return self.full_name
