b687f6cdb4afcbc6a240c60ba2c8ff3e0da66488
Маргарита <rita.limonova@list.ru>
2026-09-22 15:35:58 +0700

    Add DRF REST API for employees and departments

---
+++ employees/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import EmployeeViewSet, DepartmentViewSet
+
+
+router = DefaultRouter()
+router.register("employees", EmployeeViewSet)
+router.register("departments", DepartmentViewSet)
+
+urlpatterns = router.urls
--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Employee, Department
+from .serializers import EmployeeSerializer, DepartmentSerializer
 
-# Create your views here.
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
